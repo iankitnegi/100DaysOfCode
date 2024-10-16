da08b93d703c2a3f94e57e485a1e7ae413a1562f
Ankit Negi <132642567+iankitnegi@users.noreply.github.com>
2024-10-17 02:00:59 +0530

    Add files via upload

---
+++ Day 08/Positional vs Keyword Arguments/task.py
@@ -0,0 +1,18 @@
+# Functions with input
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+
+
+# Functions with more than 1 input:
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with("Jack", "Nowhere")
+greet_with(location="Nowhere", name="Angela")
+
