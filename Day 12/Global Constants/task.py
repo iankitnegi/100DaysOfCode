e0196ca7921ea1a9e3d21d78b6e6779eca54cc4e
Ankit Negi <132642567+iankitnegi@users.noreply.github.com>
2024-10-18 22:41:49 +0530

    Add files via upload

---
+++ Day 12/Global Constants/task.py
@@ -0,0 +1,9 @@
+PI = 3.14159
+GOOGLE_URL = "https://www.google.com"
+
+
+
+
+
+def my_fun():
+    print(GOOGLE_URL)
---
+++ Day 12/Namespaces and Scope/task.py
@@ -0,0 +1,17 @@
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+
+# Local Scope
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
+print(potion_strength)
